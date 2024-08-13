57cc4747593f4a05c319821242c649ade7f2044a
lowiejaymo <lowie.jaymier@gmail.com>
2024-08-13 22:07:34 +0800

    update fix footer

--- developer-student-portal/layout/fixed-footer.php
+++ developer-student-portal/layout/fixed-footer.php
@@ -1,4 +1,4 @@
- <!-- fixed-footer.php and footer of Webpage.
+<!-- fixed-footer.php and footer of Webpage.
 Author:
   Lowie Jay Orillo lowie.jaymier@gmail.com, 
   Caryl Mae Subaldo subaldomae29@gmail.com, 
@@ -6,6 +6,11 @@ Author:
 Last Modified: June 15, 2024
 Brief overview of the file's contents. -->
 
-<footer class="main-footer">
-    <strong>Developed by Bognot, B.A., Orillo, L.J., and Subaldo, C.M. as a requirement for the subject Integrative Programming and Technology.</strong> All rights reserved<a href="helloworld.php">.</a>
-  </footer>
+<footer class="main-footer" style="height: 60px;"> <!-- Adjust height as needed -->
+  <img src="images/developer-logo.png" class="card-img-top" alt="ComSoc Logo" style="height: 100%; width: auto;">
+  <strong>Developed by Computing Society - RMMC Gensan.</strong> All rights reserved<a href="helloworld.php">.</a>
+  <a class="">
+    <i class="nav-icon fas fa-solid fa-circle-info"></i>
+  </a>
+
+</footer>
