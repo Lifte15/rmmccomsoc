a41d04f9e93b279733c0a7abbaf167f44eb028fb
lowiejaymo <lowie.jaymier@gmail.com>
2024-08-13 22:51:22 +0800

    footer

--- cbe-student-portal/layout/fixed-footer.php
+++ cbe-student-portal/layout/fixed-footer.php
@@ -1,5 +1,9 @@
- 
-
-<footer class="main-footer">
-    <strong>Developed by Bognot, B.A., Orillo, L.J., and Subaldo, C.M. as a requirement for the subject Integrative Programming and Technology.</strong> All rights reserved<a href="helloworld.php">.</a>
-  </footer>
+<footer class="main-footer" style="height: 50px; display: flex; align-items: center; justify-content: space-between; padding: 0 20px;">
+  <div style="display: flex; align-items: center;">
+    <img src="../images/developer-logo.png" class="card-img-top" alt="ComSoc Logo" style="height: auto; max-height: 30px; width: auto;">
+    <strong style="margin-left: 10px;">Developed by Computing Society - RMMC Gensan.</strong>
+  </div>
+  <a href="http://localhost/rmmccomsoc/about.php" target="_blank" class="info-icon" style="color: inherit; text-decoration: none;">
+    <i class="nav-icon fas fa-solid fa-info"></i>
+  </a>
+</footer>
--- developer-student-portal/layout/fixed-footer.php
+++ developer-student-portal/layout/fixed-footer.php
@@ -1,16 +1,9 @@
-<!-- fixed-footer.php and footer of Webpage.
-Author:
-  Lowie Jay Orillo lowie.jaymier@gmail.com, 
-  Caryl Mae Subaldo subaldomae29@gmail.com, 
-  Brian Angelo Bognot c09651052069@gmail.com.
-Last Modified: June 15, 2024
-Brief overview of the file's contents. -->
-
-<footer class="main-footer" style="height: 60px;"> <!-- Adjust height as needed -->
-  <img src="images/developer-logo.png" class="card-img-top" alt="ComSoc Logo" style="height: 100%; width: auto;">
-  <strong>Developed by Computing Society - RMMC Gensan.</strong> All rights reserved<a href="helloworld.php">.</a>
-  <a class="">
-    <i class="nav-icon fas fa-solid fa-circle-info"></i>
+<footer class="main-footer" style="height: 50px; display: flex; align-items: center; justify-content: space-between; padding: 0 20px;">
+  <div style="display: flex; align-items: center;">
+    <img src="images/developer-logo.png" class="card-img-top" alt="ComSoc Logo" style="height: auto; max-height: 30px; width: auto;">
+    <strong style="margin-left: 10px;">Developed by Computing Society - RMMC Gensan.</strong>
+  </div>
+  <a href="http://localhost/rmmccomsoc/about.php" target="_blank" class="info-icon" style="color: inherit; text-decoration: none;">
+    <i class="nav-icon fas fa-solid fa-info"></i>
   </a>
-
 </footer>
